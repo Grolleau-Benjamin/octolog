ae2c798d274203d3bf8cc4e46a49aa0673e30829
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2026-02-03 09:41:23 +0100

    feat(chore): main program
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod app;
+pub mod cli;
+pub mod config;
+
+pub mod core;
+pub mod processing;
+pub mod runtime;
+pub mod sinks;
+pub mod sources;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Welcome to Octolog!");
+use clap::Parser;
+
+#[tokio::main]
+async fn main() {
+    let args = octolog::cli::CliArgs::parse();
+
+    if let Err(err) = octolog::app::run(args).await {
+        eprintln!("{err}");
+        std::process::exit(1);
+    }
 }
